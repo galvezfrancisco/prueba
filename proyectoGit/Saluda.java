3356282dd9951c8d7c24ac6c99504cd3b75584e8
galvezfrancisco <fgalvez@MAD.local>
2021-04-20 12:09:07 +0200

    archivos de practica

---
+++ proyectoGit/Saluda.java
@@ -0,0 +1,13 @@
+package paqueteGitHub;
+
+public class Saluda{
+
+	public static void main(String[] args){
+		System.out.println("Hola chavales, ¿cómo estáis?");
+		System.out.println("Illo hermano");
+		System.out.println("Estoy mu bien.Soy Juan Antonio López Mera");
+		System.out.println("Estoy mu bien.Soy Antonio Barrera Romero");
+		System.out.println("Yo no suplico por dieces, suplico por cincos");
+		System.out.println("Última práctica!!! Belén Santos Alonso.");
+	}
+}
